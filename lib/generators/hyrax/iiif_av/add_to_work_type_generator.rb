4b486715eb261ea2286622739de9e3ebdc6d8409
Chris Colvard <cjcolvar@indiana.edu>
2018-09-26 15:19:31 -0400

    Fix indentation of generated code.

--- lib/generators/hyrax/iiif_av/add_to_work_type_generator.rb
+++ lib/generators/hyrax/iiif_av/add_to_work_type_generator.rb
@@ -24,15 +24,15 @@ module Hyrax
         presenter_file = File.join('app/presenters/hyrax', class_path, "#{file_name}_presenter.rb")
         insert_into_file presenter_file, after: '< Hyrax::WorkShowPresenter' do
           "\n" \
-          "  # Adds behaviors for hyrax-iiif_av plugin.\n" \
-          "  include Hyrax::IiifAv::DisplaysIiifAv\n" \
-          "  Hyrax::MemberPresenterFactory.file_presenter_class = Hyrax::IiifAv::IiifFileSetPresenter\n" \
+          "    # Adds behaviors for hyrax-iiif_av plugin.\n" \
+          "    include Hyrax::IiifAv::DisplaysIiifAv\n" \
+          "    Hyrax::MemberPresenterFactory.file_presenter_class = Hyrax::IiifAv::IiifFileSetPresenter\n" \
           "\n" \
-          "  # Optional override to select iiif viewer to render\n" \
-          "  # default :avalon for audio and video, :universal_viewer for images\n" \
-          "  # def iiif_viewer\n" \
-          "  #   :avalon\n" \
-          "  # end"
+          "    # Optional override to select iiif viewer to render\n" \
+          "    # default :avalon for audio and video, :universal_viewer for images\n" \
+          "    # def iiif_viewer\n" \
+          "    #   :avalon\n" \
+          "    # end"
         end
       end
     end
