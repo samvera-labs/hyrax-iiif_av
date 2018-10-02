527d4b9ea73e5d079e320b2cc906b14b94500ac3
cjcolvar <cjcolvar@indiana.edu>
2018-10-02 10:21:18 -0400

    Remove unnecessary comments

--- app/presenters/hyrax/iiif_av/iiif_file_set_presenter.rb
+++ app/presenters/hyrax/iiif_av/iiif_file_set_presenter.rb
@@ -5,9 +5,6 @@ module Hyrax
     class IiifFileSetPresenter < ::Hyrax::FileSetPresenter
       include Hyrax::IiifAv::DisplaysContent
 
-      # FIXME: Is this safe?!? Doesn't it set a global config on load and not just when used?
-      # Hyrax::MemberPresenterFactory.file_presenter_class = Hyrax::IiifAv::IiifFileSetPresenter
-
       # Override this to include ranges in your manifest
       def range
         []
